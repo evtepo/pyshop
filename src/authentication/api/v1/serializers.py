58bf09e4db6162c2d26c26d8857d4e367b153b03
evtepo <andrey_laguta00@mail.ru>
2025-01-19 19:07:28 +1000

    auth serializers

---
+++ src/authentication/api/v1/serializers.py
@@ -0,0 +1,46 @@
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+
+from authentication.models import CustomUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ("id", "username", "email")
+
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=False, default=None)
+
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email", "password")
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create(
+            username=validated_data.get("username"),
+            email=validated_data.get("email"),
+        )
+        user.set_password(validated_data.get("password"))
+        user.save()
+
+        return user
+    
+
+class AuthJWTSerializer(serializers.Serializer):
+    email = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        email, password = attrs.get("email"), attrs.get("password")
+        if not email or not password:
+            raise serializers.ValidationError("Email and password are required.")
+
+        user = authenticate(email=email, password=password)
+        if not user:
+            raise serializers.ValidationError("Invalid email or password.")
+
+        attrs["user"] = user
+
+        return attrs
