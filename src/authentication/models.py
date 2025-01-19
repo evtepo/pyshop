d6f7affe328906d03c65f2c5f87e98da3d2873d8
evtepo <andrey_laguta00@mail.ru>
2025-01-19 19:02:17 +1000

    add refreshtoken model

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,6 +1,19 @@
+from uuid import uuid4
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True, null=True)
+
+
+class RefreshToken(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    exp_time = models.DateTimeField()
+    expire = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.id
