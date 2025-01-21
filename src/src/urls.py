08703f4c7bf13fe31fbf2b06b45af25ba2fe043e
evtepo <andrey_laguta00@mail.ru>
2025-01-21 21:36:51 +1000

    pep

--- src/src/urls.py
+++ src/src/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("authentication.api.urls"))
+    path("api/", include("authentication.api.urls")),
 ]
