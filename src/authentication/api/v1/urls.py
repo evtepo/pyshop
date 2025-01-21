ece1a039d5d494fe23f9d298644ae44c26687f88
evtepo <andrey_laguta00@mail.ru>
2025-01-21 21:35:06 +1000

    add swagger url

--- src/authentication/api/v1/urls.py
+++ src/authentication/api/v1/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
 
 from .views import LoginView, LogoutView, RefreshTokenView, RegisterView, UserProfileView
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Auth API",
+        default_version="v1",
+        description="API for authentication",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
@@ -9,4 +22,6 @@ urlpatterns = [
     path("me/", UserProfileView.as_view(), name="profile"),
     path("refresh/", RefreshTokenView.as_view(), name="refresh"),
     path("register/", RegisterView.as_view(), name="register"),
+
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
