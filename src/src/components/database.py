3a4d602f3cdece57d8b4d2bb186e78f451c03169
evtepo <andrey_laguta00@mail.ru>
2025-01-22 22:18:39 +1000

    change database

--- src/src/components/database.py
+++ src/src/components/database.py
@@ -1,10 +1,6 @@
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": os.environ.get("PGPORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
